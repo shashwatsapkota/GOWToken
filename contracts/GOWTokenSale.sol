d3168260a8c1509e17de3402db7ae7b9195db2d9
shashwatsapkota <shashwatsapkota09@gmail.com>
2018-05-23 14:22:39 +0545

    Added Buy Tokens

--- contracts/GOWTokenSale.sol
+++ contracts/GOWTokenSale.sol
@@ -7,7 +7,10 @@ contract GOWTokenSale{
     address admin;
     GOWToken public tokenContract;
     uint256 public tokenPrice;
+    uint256 public tokensSold;
    
+    event Sell(address _buyer, uint256 _amount);
+
     constructor (GOWToken _tokenContract, uint256 _tokenPrice) public {
         // Assign an admin
         admin = msg.sender;
@@ -15,6 +18,24 @@ contract GOWTokenSale{
         tokenContract = _tokenContract;
         // Token Price
         tokenPrice = _tokenPrice;
+    }
+
+    //multiply
+    function  multiply(uint x, uint y) internal pure returns (uint z){
+        require(y == 0 || (z = x*y) / y == x);
+    }
+
+    function buyTokens(uint256 _numberOfTokens) public payable {
+        //Require that value is equal to tokens
+        require(msg.value == multiply(_numberOfTokens, tokenPrice));
+        //Require that the contract has enough tokens
+        require(tokenContract.balanceOf(this) >= _numberOfTokens);
+        //Require that a transfer is successful
+        require(tokenContract.transfer(msg.sender , _numberOfTokens));
+        //Keep trak of tokensSold
+        tokensSold += _numberOfTokens;
+        //Trigger Sell Event
+        emit Sell(msg.sender, _numberOfTokens);
     }
 
 }
--- test/GOWTokenSale.js
+++ test/GOWTokenSale.js
@@ -1,8 +1,15 @@
 var GOWTokenSale = artifacts.require("./GOWTokenSale.sol")
+var GOWToken = artifacts.require("./GOWToken.sol")
 
 contract('GOWTokenSale', function(accounts){
     var tokenSaleInstace;
+    var tokenInstance;
+    var buyer = accounts[1];
+    var admin = accounts[0];
+    var tokensAvailable = 750000;
     var tokenPrice = 1000000000000000;
+    var numberOfTokens;
+            
 
     it('initializes the contract with the correct values',function(){
         return GOWTokenSale.deployed().then(function(instance){
@@ -16,6 +23,40 @@ contract('GOWTokenSale', function(accounts){
             return tokenSaleInstace.tokenPrice();
         }).then(function(price){
             assert.equal(price,tokenPrice, 'token price is correct');
+        });
+    });
+
+    it('facilitates token buying', function(){
+        return GOWToken.deployed().then(function(instance){
+            tokenInstance = instance;
+            return GOWTokenSale.deployed();
+        }).then(function(instance){
+            tokenSaleInstace = instance;
+            return tokenInstance.transfer(tokenSaleInstace.address, tokensAvailable, { from: admin });
+        }).then(function(receipt){
+            numberOfTokens=10;
+            var value = numberOfTokens * tokenPrice;
+            return tokenSaleInstace.buyTokens(numberOfTokens, { from: buyer, value: value})
+        }).then(function(receipt){
+            assert.equal(receipt.logs.length, 1, 'triggers one event');
+            assert.equal(receipt.logs[0].event, 'Sell', 'should be the "Sell" event');
+            assert.equal(receipt.logs[0].args._buyer, buyer, 'logs the account that purchassed the tokens');
+            assert.equal(receipt.logs[0].args._amount, numberOfTokens, 'logs the number of tokens purchased');
+            return tokenSaleInstace.tokensSold();
+        }).then(function(amount){
+            assert.equal(amount.toNumber(), numberOfTokens, 'icrements the number of tokens sold');
+            return tokenInstance.balanceOf(buyer);
+        }).then(function(balance){
+            assert.equal(balance.toNumber(), numberOfTokens)
+            return tokenInstance.balanceOf(tokenSaleInstace.address);
+        }).then(function(balance){
+            assert.equal(balance.toNumber(), tokensAvailable - numberOfTokens, '')
+            return tokenSaleInstace.buyTokens(numberOfTokens, { from: buyer, value: 1});
+        }).then(assert.fail).catch(function(error){
+            assert(error.message.indexOf('revert')>=0, 'msg.value must euqal numer of tokens in wei')
+            return tokenSaleInstace.buyTokens(800000, { from: buyer, value: 1});
+        }).then(assert.fail).catch(function(error){
+            assert(error.message.indexOf('revert')>=0, 'cannot transfer more than avaialbale')
         });
     });
 });
